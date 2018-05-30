fb0c8a9e9e530579a84972ce7f44c1961fb1c69d
Lev Vasilyev <wanbergg@gmail.com>
2018-05-30 17:36:03 +0300

    Task-2 bugfix
    UPD: Seems that date object cant handle all the task, will try to to this by hands by adding  Math.ceil() method to variable 'res' ; added variable msPerDay for code to be more readable; switched months to be constant;

--- task2.js
+++ task2.js
@@ -1,10 +1,11 @@
 /* Task 2 */
-let months = { "января": 0, "февраля": 1, "марта": 2, "апреля": 3, "мая": 4, "июня": 5, "июля": 6, "августа": 7, "сентября": 8, "октября": 9, "ноября": 10, "декабря": 11 };
+const months = { "января": 0, "февраля": 1, "марта": 2, "апреля": 3, "мая": 4, "июня": 5, "июля": 6, "августа": 7, "сентября": 8, "октября": 9, "ноября": 10, "декабря": 11 };
 let day = process.argv[1];
 let month = months[process.argv[2]];
 let year = process.argv[3];
 let initialDate = new Date(year, month, day);
 let finalDate = new Date(year, 11, 31);
-let res = (finalDate - initialDate) / 86400000;
-process.stdout.write(res + '\n');
+const msPerDay = 24*60*60*1000;
+let res = Math.ceil((finalDate.getTime() - initialDate.getTime())/msPerDay);
+process.stdout.write(res.toString());
 /* Put your code here */
